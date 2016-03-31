f0c313bfd86ece6c6414094b4e4b7c47c6d4f249
alexbeno <alexbenoliel@gmail.com>
2016-03-31 21:27:06 +0200

    mise en forme des exo
    premiere exercice
    main.js pour changement de template
    boostrap
    changement de styles

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  
+  $("#valideP2").on("click",function(e){ 
+    $( "#template1").css( "display", "none");
+    $( "#template2").css( "display", "block");
+ 
+  
+  });
+  });
