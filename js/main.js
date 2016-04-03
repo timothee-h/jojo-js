3632e99a4da4ab96d947a4b5c7b19602b118b741
alexbeno <alexbenoliel@gmail.com>
2016-04-03 14:40:10 +0200

    Cour

--- js/main.js
+++ js/main.js
@@ -67,10 +67,37 @@ $(document).ready(function(){
 	
 	/*
 	
-	
+	**********cour************
 	
 	*/
 	
-	
+	    $("#menuCour > ul > li > p").click(function(){
+    $("#menuCour").addClass("col-md-2 col-sm-2 col-xs-2")
+  });
+  
+  
+  $(".sousMenu").hide();
+
+  $(".titreMenuCour").on("click",function(){
+    if($(this).not($(this)).hasClass('activeS')){
+      $('.activeS').removeClass("activeS").slideToggle();
+    }
+    else{
+      $('.activeS').removeClass("activeS").slideToggle();
+      $(this).next('.sousMenu').addClass("activeS").slideToggle();
+    }
+  });
+  
+    $(".ContenuCour").hide();
+
+  $(".titreSousMenu").on("click",function(){
+    if($(this).not($(this)).hasClass('activeC')){
+      $('.activeC').removeClass("activeC").slideToggle();
+    }
+    else{
+      $('.activeC').removeClass("activeC").slideToggle();
+      $(this).next('.ContenuCour').addClass("activeC").slideToggle();
+    }
+  });
 	
 });
